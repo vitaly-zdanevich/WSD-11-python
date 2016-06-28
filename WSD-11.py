bb26f7692aa39c4c323a200e475bf600ff9a0c5e
Vitaly Zdanevich <vitaly.zdanevich@waveoc.com>
2016-06-28 16:10:35 +0300

    done showing text from archive

--- WSD-11.py
+++ WSD-11.py
@@ -1,7 +1,10 @@
 import json
+import zipfile
+from io import BytesIO
 
 import flask
 from flask import Flask
+
 import requests
 
 app = Flask(__name__)
@@ -9,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/show_text_file')
 def show_text_file():
-    req = requests.get('https://stash.waveoc.com/rest/api/1.0/projects/WIR/repos/python-learning/browse/text_file.txt?'\
+    req = requests.get('https://stash.waveoc.com/rest/api/1.0/projects/WIR/repos/python-learning/browse/text_file.txt?'
                        'at=refs/heads/feature/files')
     jsn = json.loads(req.text)
 
@@ -19,6 +22,16 @@ def show_text_file():
 
     return flask.Response(text, status=200, mimetype='application/json')
 
+
+@app.route('/show_archive_files')
+def show_archive_files():
+    req = requests.get('https://stash.waveoc.com/projects/WIR/repos/python-learning/browse/Archive.zip'
+                       '?at=refs/heads/feature/files&raw')
+
+    archive = zipfile.ZipFile(BytesIO(req.content))
+
+    with archive.open('Opportunity_Trigger.cls') as myfile:
+        return flask.Response(myfile.read(), status=200, mimetype='text/text')
 
 if __name__ == '__main__':
     app.run()
